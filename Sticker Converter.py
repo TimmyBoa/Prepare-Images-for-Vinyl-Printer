55d583e517d9ff78bb7953033152cd2d2ea46ea4
Timothy Pollard <107521584+TimmyBoa@users.noreply.github.com>
2025-01-13 18:21:36 -0500

    Update Sticker Converter.py
    Iterate through file extensions with a for loop instead of manual searching simplifying code and increasing readability.

--- Sticker Converter.py
+++ Sticker Converter.py
@@ -17,7 +17,6 @@ def process_file(filename, extension, remove_background):
         file_path = os.path.join(directory, filename)
         if os.path.exists(file_path):
             im = Image.open(file_path)
-            
             if (extension==".gif"):
                 im.seek(0)
             if (remove_background==True):
@@ -30,21 +29,10 @@ def process_file(filename, extension, remove_background):
             
 if __name__=="__main__":  
     directory = r'C:\Users\Tim\Pictures\Stickers'
-    remove_background=True
+    remove_background=False
+    file_extensions=[".png",".jpg",".jpeg",".gif",".jfif",".webp",".bmp"]
     for filename in os.listdir(directory):
-        if filename.endswith(".png"):
-            process_file(filename, ".png", remove_background)
-        elif filename.endswith(".jpg"):
-            process_file(filename, ".jpg", remove_background)
-        elif filename.endswith(".jpeg"):
-            process_file(filename, ".jpeg", remove_background)
-        elif filename.endswith(".gif"):
-            process_file(filename, ".gif", remove_background)
-        elif filename.endswith(".jfif"):
-            process_file(filename, ".jfif", remove_background)
-        elif filename.endswith(".webp"):
-            process_file(filename, ".webp", remove_background)
-        elif filename.endswith(".bmp"):
-            process_file(filename, ".bmp", remove_background)
-
-    
+        for extension in file_extensions:
+            if filename.endswith(extension):
+                process_file(filename, extension, remove_background)   
+                break
